6caca81dab24a1b726bb3319dffd49b836d1d9da
Usama-dev1 <raja.usamah@gmail.com>
2024-08-08 11:26:40 +0500

    fixed styles

--- src/components/HeroArea.jsx
+++ src/components/HeroArea.jsx
@@ -11,14 +11,14 @@ const HeroArea = () => {
             alt=""
             className="h-32 md:h-64 mb-4 md:mb-0 md:mr-4 object-contain"
           />
-          <div className="flex flex-col items-center w-full md:w-auto px-4 md:px-0">
-            <h2 className="text-secondary text-center text-xl md:text-2xl font-bold mb-4">
+          <div className="flex flex-col justify-center items-center sm:items-start w-full md:w-auto px-4 md:px-0">
+            <h2 className="text-secondary flex i text-xl md:text-4xl font-bold mb-4">
               Enter the URL to Shorten!
             </h2>
             <div className="flex flex-col md:flex-row items-center w-full md:w-[40rem]">
               <input
                 type="text"
-                className="rounded-2xl sm:rounded-none md:rounded-l-xl placeholder:text-white bg-green-900 opacity-80 shadow-2xl border border-green-900 w-full md:w-[25rem] py-4 px-4 mb-2 md:mb-0"
+                className="rounded-2xl sm:rounded-none md:rounded-l-xl placeholder:text-white bg-green-900 opacity-80 shadow-2xl border text-white text-md border-green-900 w-full md:w-[25rem] py-4 px-4 mb-2 md:mb-0"
                 placeholder="Paste a link to shorten it"
               />
               <button className="bg-sorange sm:rounded-none rounded-2xl md:rounded-r-xl text-white px-6 py-4 text-lg font-semibold hover:bg-orange-600 transition-colors">
